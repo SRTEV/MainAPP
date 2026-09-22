e8a593595342eba7faf41d29a9fe59b2ce31848a
System-500 <s101762@pollub.edu.pl>
2026-09-22 18:18:23 +0200

    fixed syntax error in repairman profile page

--- lib/RepairmanViewModels/RepairmanProfilePage.dart
+++ lib/RepairmanViewModels/RepairmanProfilePage.dart
@@ -105,9 +105,10 @@ class _RepairmanProfileState extends State<RepairmanProfile> {
                         ),
                       ),
                     ],
-                  ]),,
-              )const Spacer(flex: 3),
-              Align(
+                  ],
+                ),
+              ),
+              const Spacer(flex: 3)Align(
                 alignment: Alignment.centerRight,
                 child: TextButton(
                   onPressed: () {
